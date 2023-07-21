469bc653d865b338aca1aca6d37e3d0ef56ae652
Gabriel Lulu <glulu4444@gmail.com>
2023-07-21 15:43:19 -0400

    fixed the package issues

---
+++ portfolio/src/App.js
@@ -0,0 +1,37 @@
+import './App.css';
+
+
+import Home from './Pages/Home/Home'
+import { Routes, Route } from 'react-router-dom';
+import Bagels from './Pages/Slider/Bagels/Bagels.js';
+import Neighborhood from './Pages/Slider/Neighborhood';
+import Sorts from './Pages/Slider/Sorts/Sorts.js';
+import VmSim from './Pages/Slider/VmSim';
+import AirlineSystem from './Pages/Slider/Airline/AirlineSystem';
+import Tbd from './Pages/Slider/Tbd.js'
+import Portfolio from './Pages/Slider/Portfolio/Portfolio';
+
+
+function App() {
+  return (
+
+    <Routes>
+
+      <Route path="/" element={<Home />} />
+      <Route path="/airline" element={<AirlineSystem />} />
+      <Route path="/vmsim" element={<VmSim />} />
+      <Route path="/sorting-algorithms" element={<Sorts />} />
+      <Route path="/neighborhood" element={<Neighborhood />} />
+      <Route path="/bagels" element={<Bagels />} />
+      <Route path="/tbd" element={<Tbd />} />
+      <Route path="/portfolio" element={<Portfolio />} />
+
+
+    </Routes>
+
+
+  );
+}
+
+export default App;
+
---
+++ portfolio/src/Pages/Slider/MySlider.js
@@ -0,0 +1,52 @@
+import { motion } from 'framer-motion';
+import React, { useEffect, useState, useRef } from 'react';
+import projectCards from '../Slider/Cards'
+
+
+import './MySlider.css'
+
+
+
+
+
+function MySlider() {
+
+    // eslint-disable-next-line
+    const [width, setWidth] = useState(0);
+    const carousel = useRef();
+
+
+    useEffect(() => {
+
+
+
+        setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth)
+    }, [])
+
+
+
+
+
+
+
+
+
+    return (
+        <div className='slider'>
+            {/* drag='x' add to second div with the numbers ans shit */}
+            <motion.div ref={carousel} whileTap={{ cursor: 'grabbing' }} className='carousel' >
+                <motion.div className='inner-carousel' style={{ touchAction: 'pan-x' }}>
+                    {projectCards.map((card, index) => {
+                        return (
+                            <motion.div key={index} className='item'>
+                                {card}
+                            </motion.div>
+                        )
+                    })}
+                </motion.div>
+            </motion.div>
+        </div>
+    );
+
+}
+export default MySlider;
