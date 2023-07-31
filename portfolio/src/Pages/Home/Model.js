a25998c2ac42fda4f7ff0abd74cbe5e274ad82e1
Gabriel Lulu <glulu4444@gmail.com>
2023-07-31 09:45:13 -0400

    optimized model

--- portfolio/src/Pages/Home/Model.js
+++ portfolio/src/Pages/Home/Model.js
@@ -33,118 +33,88 @@ export default function Model(props) {
     return (
         <group {...props} dispose={null}>
             <group rotation={[-Math.PI / 2, 0, 0]}>
-                    <mesh
-                        castShadow
-                        receiveShadow
+                    {/* <mesh
                         scale={0.5}
                         
                         geometry={nodes.Object_2.geometry}
                         material={materials["Scene_-_Root"]}
-                    />
-                    <mesh
-                        castShadow
-                        receiveShadow
+                    /> */}
+                    {/* <mesh
                         scale={0.5}
                         
                         geometry={nodes.Object_3.geometry}
                         material={materials["Scene_-_Root"]}
-                    />
-                    <mesh
-                        castShadow
+                    /> */}
+                    {/* <mesh
                         scale={0.5}
 
-                        receiveShadow
                         geometry={nodes.Object_4.geometry}
                         material={materials["Scene_-_Root"]}
-                    />
+                    /> */}
                     <mesh
-                        castShadow
                         scale={0.5}
 
-                        receiveShadow
                         geometry={nodes.Object_5.geometry}
                         material={materials["Scene_-_Root"]}
                     />
                     <mesh
-                        castShadow
                         scale={0.5}
 
-                        receiveShadow
                         geometry={nodes.Object_6.geometry}
                         material={materials["Scene_-_Root"]}
                     />
                     <mesh
-                        castShadow
                         scale={0.5}
 
-                        receiveShadow
                         geometry={nodes.Object_7.geometry}
                         material={materials["Scene_-_Root"]}
                     />
                     <mesh
-                        castShadow
                         scale={0.5}
 
-                        receiveShadow
                         geometry={nodes.Object_8.geometry}
                         material={materials["Scene_-_Root"]}
                     />
                     <mesh
-                        castShadow
                         scale={0.5}
 
-                        receiveShadow
                         geometry={nodes.Object_9.geometry}
                         material={materials["Scene_-_Root"]}
                     />
                     <mesh
-                        castShadow
                         scale={0.5}
 
-                        receiveShadow
                         geometry={nodes.Object_10.geometry}
                         material={materials["Scene_-_Root"]}
                     />
                     <mesh
-                        castShadow
                         scale={0.5}
 
-                        receiveShadow
                         geometry={nodes.Object_11.geometry}
                         material={materials["Scene_-_Root"]}
                     />
                     <mesh
-                        castShadow
                         scale={0.5}
 
-                        receiveShadow
                         geometry={nodes.Object_12.geometry}
                         material={materials["Scene_-_Root"]}
                     />
-                    <mesh
-                        castShadow
+                    {/* <mesh
                         scale={0.5}
 
-                        receiveShadow
                         geometry={nodes.Object_13.geometry}
                         material={materials["Scene_-_Root"]}
-                    />
-                    <mesh
-                        castShadow
+                    /> */}
+                    {/* <mesh
                         scale={0.5}
-
-                        receiveShadow
                         geometry={nodes.Object_14.geometry}
                         material={materials["Scene_-_Root"]}
                     />
                     <mesh
-                        castShadow
                         scale={0.5}
-
-                        receiveShadow
                         geometry={nodes.Object_15.geometry}
                         material={materials["Scene_-_Root"]}
-                    />
+                    /> */}
 
             </group>
         </group>
